1e3980d6f236cc5a68eb77bfae757b3faf3d9d38
JPreault <jose@preault.com>
2024-04-22 20:27:29 +0200

    homepage

--- src/components/Skeletton.jsx
+++ src/components/Skeletton.jsx
@@ -25,38 +25,39 @@ const Skeletton = () => {
     }
 
     return (<div className="Skeletton">
-        <div className='header'>
-            <ContainLogo className={`Logo ${!onPortfolio && 'spa'}`} onClick={() => navigate(links.home)}>
-                <Logo />
-            </ContainLogo>
-            {onPortfolio && <div className= "separator hiddenOnTouch" />}
-            <div className='Menu'>
-                {onPortfolio && <>
-                    <Link className="hiddenOnTouch" selected={pathname === links.about} link={links.about} text={<><span>a</span> propos</>} />
-                    <Link className="hiddenOnTouch" selected={pathname === links.skills} link={links.skills} text={<><span>m</span>es expériences<br />et mes compétences</>} />
-                    <Link className="hiddenOnTouch" selected={pathname === links.professionalActions} link={links.professionalActions} text={<><span>m</span>es actions<br />professionnelles</>} />
-                    <Link className="hiddenOnTouch" selected={pathname === links.interests} link={links.interests} text={<><span>m</span>es centres<br />d'intérêt</>} />
-                    <Link className="hiddenOnTouch" selected={pathname === links.contact} link={links.contact} text={<><span>c</span>ontact</>} />
-                </>}
-                <ContainLogo className="immersive" onClick={changeInterface}>
-                    <Immersive />
-                    <div className={`circle ${!onPortfolio && 'spa'}`}/>
+        <div className='pagesContainer'>
+            <div className='header'>
+                <ContainLogo className={`Logo ${!onPortfolio && 'spa'}`} onClick={() => navigate(links.home)}>
+                    <Logo />
                 </ContainLogo>
+                {onPortfolio && <div className="separator hiddenOnTouch" />}
+                <div className='Menu'>
+                    {onPortfolio && <>
+                        <Link className="hiddenOnTouch" selected={pathname === links.about} link={links.about} text={<><span>a</span> propos</>} />
+                        <Link className="hiddenOnTouch" selected={pathname === links.skills} link={links.skills} text={<><span>m</span>es expériences<br />et mes compétences</>} />
+                        <Link className="hiddenOnTouch" selected={pathname === links.professionalActions} link={links.professionalActions} text={<><span>m</span>es actions<br />professionnelles</>} />
+                        <Link className="hiddenOnTouch" selected={pathname === links.interests} link={links.interests} text={<><span>m</span>es centres<br />d'intérêt</>} />
+                        <Link className="hiddenOnTouch" selected={pathname === links.contact} link={links.contact} text={<><span>c</span>ontact</>} />
+                    </>}
+                    <ContainLogo className="immersive" onClick={changeInterface}>
+                        <Immersive />
+                        <div className={`circle ${!onPortfolio && 'spa'}`} />
+                    </ContainLogo>
+                </div >
             </div >
-        </div >
-        <Outlet />
-        <div className='footer'></div>
+            <Outlet />
+            <div className='footer'></div>
+        </div>
         <div className={`slideMenu ${!openMenu ? 'open' : ''}`}>
             <div className='navigationMenu'>
 
             </div>
             <div className='displayButton' onClick={() => setOpenMenu(!openMenu)}>
-                <BackgroundMenu/>
-                <i className={`icon fp-3x fpl fp-chevron-right ${!openMenu ? 'open' : ''}`}/>
+                <BackgroundMenu />
+                <i className={`icon fp-3x fpl fp-chevron-right ${!openMenu ? 'open' : ''}`} />
             </div>
         </div>
-
-    </div >);
+    </div>);
 }
 
 const Link = (data) => {
--- src/view/portfolio/Homepage.jsx
+++ src/view/portfolio/Homepage.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../../components/Button';
 import ContainLogo from '../../components/ContainLogo';
 import PageLayout from '../../components/layout/PageLayout';
@@ -5,21 +6,29 @@ import { ReactComponent as Bienvenue } from '../../images/icones/Bienvenue.svg';
 import LucilePortrait from '../../images/LucileHomepage.png';
 
 const Homepage = () => {
+    const navigate = useNavigate();
 
     return (<PageLayout className='Homepage'>
         <div className='Homepage__top'>
-            <div className='portrait'>
-                <div className='Lucile'>
-                    <img alt='Portrait de Lucile' src={LucilePortrait}/>
+            <div className='containPortraitTitle'>
+                <div className='portrait'>
+                    <div className='Lucile'>
+                        <img alt='Portrait de Lucile' src={LucilePortrait} />
+                    </div>
+                    <ContainLogo className="bienvenue">
+                        <Bienvenue />
+                    </ContainLogo>
                 </div>
-                <ContainLogo className="bienvenue">
-                    <Bienvenue/>
-                </ContainLogo>
+                <p className='titlePortfolio'>Portfolio</p>
             </div>
-            <p className='titlePortfolio'>Portfolio</p>
-            <p className='titleBeauty'>Beauté<br/>bien-être</p>
+            <div className='titleBeauty'>
+                <div className='containText'>
+                    <p className='first'>Beauté</p>
+                    <p className='second'>Bien-être</p>
+                </div>
+            </div>
         </div>
-        <Button value="En apprendre d'avantage sur moi !"/>
+        <Button value="En apprendre d'avantage sur moi !" onClick={() => navigate('/portfolio/about')} />
     </PageLayout>);
 }
 
