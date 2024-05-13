765e7cc8f3edfc914a2230f7ffc9ba265f5938c5
JPreault <jose@preault.com>
2024-05-13 13:15:40 +0200

    Feat: ajout de la page centre d'intéret

--- src/view/portfolio/Interest.jsx
+++ src/view/portfolio/Interest.jsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import PageLayout from "../../components/layout/PageLayout"
 import equitation from '../../images/interest/equitation.png';
 import equitation1 from '../../images/interest/equitation1.png';
@@ -12,6 +13,28 @@ import ski1 from '../../images/interest/ski1.png';
 import ski2 from '../../images/interest/ski2.png';
 
 const Interest = () => {
+    const [open, setOpen] = useState(false);
+    const [source, setSource] = useState(equitation);
+    const [type, setType] = useState('image');
+    const popup = useRef(null);
+
+    //useOnClickOutside(open, popup, () => setOpen(false));
+
+    function openMedia(event, source, type) {
+        event.preventDefault();
+        event.stopPropagation();
+        if(!open) {
+            setSource(source);
+            setType(type);
+            setOpen(true);
+        }
+    };
+
+    function needClose(evt) {
+        if(evt.target !== popup.current) {
+            setOpen(false);
+        };
+    }
 
     return (<PageLayout className='Interest'>
         <div className="Interest__equitation">
@@ -19,37 +42,59 @@ const Interest = () => {
                 <p className="title">équitation</p>
                 <div className="leftPart">
                     <div className="top">
-                        <video controls>
-                            <source src={equitation3} type="video/mp4" />
-                        </video>
-                        <img src={equitation2} />
+                        <div className="equitation1">
+                            <div className="play" onClick={(event)=> openMedia(event,equitation3, "video")}>
+                                <i className="fps fp-play-rounded"></i>
+                            </div>
+                            <video alt="Equitation1">
+                                <source src={equitation3 + "#t=0.1"} type="video/mp4" />
+                            </video>
+                        </div>
+                        <div className="equitation2">
+                            <img alt="Equitation2" src={equitation2} onClick={(event)=> openMedia(event, equitation2, "image")} />
+                        </div>
                     </div>
                     <div className="bottom">
-                        <img src={equitation4} />
-                        <img src={equitation1} />
+                        <img className="equitation3" alt="Equitation3" src={equitation4} onClick={(event)=> openMedia(event,equitation4, "image")} />
+                        <img className="equitation4" alt="Equitation4" src={equitation1} onClick={(event)=> openMedia(event,equitation1, "image")} />
                     </div>
                 </div>
-                <img src={equitation} />
+                <div className="rightPart">
+                    <img className="equitation5" alt="Equitation5" src={equitation} onClick={(event)=> openMedia(event,equitation, "image")} />
+                </div>
             </div>
         </div>
         <div className="Interest__ski">
             <p className="title">ski</p>
             <div className="Interest__ski--containMedia">
-                <img src={ski2} />
-                <img src={ski1} />
-                <video controls>
-                    <source src={ski} type="video/mp4" />
-                </video>
+                <div className="ski1">
+                    <div className="play" onClick={(event)=> openMedia(event,ski, "video")}>
+                        <i className="fps fp-play-rounded"></i>
+                    </div>
+                    <video alt="Ski1">
+                        <source src={ski + "#t=0.1"} type="video/mp4" />
+                    </video>
+                </div>
+                <img alt="Ski2" className="ski2" src={ski2} onClick={(event)=> openMedia(event,ski2, "image")} />
+                <img alt="Ski3" className="ski3" src={ski1} onClick={(event)=> openMedia(event,ski1, "image")} />
             </div>
         </div>
         <div className="Interest__musculation">
             <p className="title">musculation</p>
             <div className="Interest__musculation--containMedia">
-                <img src={musculation1} />
-                <img src={musculation} />
-                <img src={musculation2} />
+                <img alt="Musculation1" className="musculation1" src={musculation2} onClick={(event)=> openMedia(event,musculation2, "image")} />
+                <img alt="Musculation2" className="musculation2" src={musculation} onClick={(event)=> openMedia(event,musculation, "image")} />
+                <img alt="Musculation3" className="musculation3" src={musculation1} onClick={(event)=> openMedia(event,musculation1, "image")} />
             </div>
         </div>
+        {open && <div className="popup" onClick={(evt) => needClose(evt)}>
+            {type === "image"
+                ? <img ref={popup} alt="ImageOuverte" src={source}/>
+                : <video ref={popup} controls>
+                    <source src={source + "#t=0.1"} type="video/mp4" />
+                </video>
+            }
+        </div>}
     </PageLayout>)
 }
 
